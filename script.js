9bc18fef9f541372593e914876d09d81031bff5e
Muhammaddavron <muhammaddavron123@gmail.com>
2024-08-03 10:19:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let name = prompt('ismingizni kiriting')
+
+let surname = prompt('familiyangizni kiriting')
+
+let age = prompt('yoshingizni kiriting')
+
+let number = prompt('raqamni kirit')
+
+let maktab = prompt('maktabni kirit')
+
+let sana = prompt('yil sana')
+
+let manzil = prompt('manzilni k')
+
+let MarsdagiUstoz = prompt('Ustozingizni ismi')
+
+console.log(name)
+console.log(surname)
+console.log(age)
+console.log(number)
+console.log(maktab)
+console.log(sana)
+console.log(manzil)
+console.log(MarsdagiUstoz)
